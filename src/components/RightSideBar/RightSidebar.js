1287b06a1ad30d3461c824b4ba9df785c8bee044
Dimitris <jimkatompao@gmail.com>
2022-01-18 21:32:57 +0200

    created a new component for tables and added it to the Home page

--- src/components/RightSideBar/RightSidebar.js
+++ src/components/RightSideBar/RightSidebar.js
@@ -1,34 +1,10 @@
 import { React, useState, useEffect } from 'react'
-import { formatNumber, sort } from '../../utils.js'
+import { sort } from '../../utils.js'
+import Table from '../Table/Table.js'
 import styles from './RightSidebar.module.scss'
-import previous from '../../assets/left-arrow.png'
-import next from '../../assets/right-arrow.png'
 import LineChart from '../Charts/Line/LineChart.js'
 
-function RightSidebar({ countries, vaccinations, metric }) {
-
-    let statsArray = [
-        {
-            name: 'activeCases',
-            title: 'Live Cases by Country',
-            index: 0
-        },
-        {
-            name: 'totalCases',
-            title: 'Total Cases by Country',
-            index: 1
-        },
-        {
-            name: 'totalDeaths',
-            title: 'Total Deaths by Country',
-            index: 2
-        },
-        {
-            name: 'totalRecovered',
-            title: 'Total Recovered by Country',
-            index: 3
-        }
-    ]
+function RightSidebar({ countries, metric }) {
 
     const chartTitle = {
         cases: 'Worldwide New Cases',
@@ -36,62 +12,57 @@ function RightSidebar({ countries, vaccinations, metric }) {
         recovered: 'Worldwide Recovered'
     }
 
-    const [stats, setStats] = useState(statsArray[0])
-    const [activeCases, setActiveCases] = useState([])
-    const [totalCases, setTotalCases] = useState([])
-    const [totalDeaths, setTotalDeaths] = useState([])
-    const [totalRecovered, setTotalRecovered] = useState([])
-    const [currentTable, setCurrentTable] = useState([])
     const [chartData, setChartData] = useState({})
+    const [tableData, setTableData] = useState({})
 
     useEffect(() => {
-        const unsortedCases = countries.map(c => {
-            return {
-                name: c.name,
-                value: c.active
-            }
-        })
-        setActiveCases(sort(unsortedCases))
-        setCurrentTable(activeCases)
+        const data = {
+            tables: {
+                activeCases: {
+                    title: 'Live Cases by Country',
+                    index: 0,
+                    data: sort(countries.map(c => {
+                        return {
+                            key: c.name,
+                            value: c.active
+                        }
+                    }))
+                },
+                totalDeaths: {
+                    title: 'Total Deaths by Country',
+                    index: 1,
+                    data: sort(countries.map(c => {
+                        return {
+                            key: c.name,
+                            value: c.deaths
+                        }
+                    }))
+                },
+                totalRecovered: {
+                    title: 'Total Recovered by Country',
+                    index: 2,
+                    data: sort(countries.map(c => {
+                        return {
+                            key: c.name,
+                            value: c.recovered
+                        }
+                    }))
+                },
+                totalCases: {
+                    title: 'Total Cases by Country',
+                    index: 3,
+                    data: sort(countries.map(c => {
+                        return {
+                            key: c.name,
+                            value: c.cases
+                        }
+                    }))
+                }
+            },
+            defaultTable: 'activeCases'
+        }
+        setTableData(data)
     }, [countries])
-
-    useEffect(() => {
-        const unsortedCases = countries.map(c => {
-            return {
-                name: c.name,
-                value: c.cases
-            }
-        })
-        setTotalCases(sort(unsortedCases))
-    }, [countries])
-
-    useEffect(() => {
-        const unsortedCases = countries.map(c => {
-            return {
-                name: c.name,
-                value: c.deaths
-            }
-        })
-        setTotalDeaths(sort(unsortedCases))
-    }, [countries])
-
-    useEffect(() => {
-        const unsortedCases = countries.map(c => {
-            return {
-                name: c.name,
-                value: c.recovered
-            }
-        })
-        setTotalRecovered(sort(unsortedCases))
-    }, [countries])
-
-    // when stats update in changeStatistics, set the new currentTable
-    useEffect(() => {
-        stats.name === 'totalDeaths' ? setCurrentTable(totalDeaths)
-        : stats.name === 'totalCases' ? setCurrentTable(totalCases)
-        : stats.name === 'activeCases' ? setCurrentTable(activeCases)
-        : setCurrentTable(totalRecovered)
-    }, [stats])
 
     useEffect(() => {
         fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=365')
@@ -102,40 +73,11 @@ function RightSidebar({ countries, vaccinations, metric }) {
         })
     }, [])
 
-    const changeStatistics = (action) => {
-        if (action === 'next') {
-            // get back to the first stat, if the current is the last one
-            if (stats.index === statsArray.length - 1) return setStats(statsArray[0])
-            setStats(statsArray[stats.index + 1])
-        } else {
-            // go to the last stat, if the current is the first one
-            if (stats.index === 0) return setStats(statsArray[statsArray.length - 1])
-            setStats(statsArray[stats.index - 1])
-        }
-    }
 
     return (
         <div className={styles.card}>
             <div className={styles.table}>
-                <div className={styles.title}>
-                    <img onClick={() => changeStatistics('previous')} src={previous} alt='previous'></img>
-                    <h1>{stats.title}</h1>
-                    <img onClick={() => changeStatistics('next')} src={next} alt='next'></img>
-                </div>
-                <div className={styles.countryRow}>
-                    <table>
-                        <tbody>
-                            {currentTable.map((country) => (
-                                <tr key={country.name}>
-                                    <td>{country.name}</td>
-                                    <td>
-                                        <strong>{formatNumber(country.value)}</strong>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
+                <Table data={tableData}/>
             </div>
             <div className={styles.graph}>
                 <div className={styles.title}>{chartTitle[metric]}</div>
---
+++ src/components/Table/Table.js
@@ -0,0 +1,77 @@
+import { React } from 'react'
+import { nanoid } from 'nanoid'
+import { formatNumber } from '../../utils.js'
+import styles from './Table.module.scss'
+import { useState, useEffect } from 'react'
+
+function Table ({data}) {
+    const [currentTable, setCurrentTable] = useState({})
+
+    useEffect(() => {
+        if (Object.keys(data).length > 0) {
+            const { defaultTable } = data
+            setCurrentTable(data.tables[defaultTable])
+        }
+    }, [data])
+    
+    const changeStatistics = (action) => {
+        const totalTables = Object.keys(data.tables).length
+        const tables = Object.values(data.tables)
+        if (action === 'next') {
+            // get back to the first stat, if the current is the last one
+            if (currentTable.index === totalTables - 1) return setCurrentTable(tables.find(t => t.index === 0))
+            return setCurrentTable(tables.find(t => t.index === currentTable.index + 1))
+        } else {
+            // go to the last stat, if the current is the first one
+            if (currentTable.index === 0) return setCurrentTable(tables.find(t => t.index === totalTables - 1))
+            return setCurrentTable(tables.find(t => t.index === currentTable.index + - 1))
+        }
+    }
+
+    const renderMultipleTables = () => {
+        return (
+            <div className={styles.title}>
+                <i
+                    className='material-icons'
+                    onClick={() => changeStatistics('previous')}
+                >
+                    arrow_back_ios
+                </i>
+                <h1>{currentTable.title}</h1>
+                <i
+                    className='material-icons'
+                    onClick={() => changeStatistics('next')}
+                >
+                    arrow_forward_ios
+                </i>
+            </div>
+        )
+    }
+
+    return (
+        <div className={styles.container}>
+            {
+                Object.keys(data).length > 1 ? renderMultipleTables() : <h1>{currentTable.title}</h1>
+            }
+            <div className={styles.table}>
+                {
+                    Object.keys(data).length > 1 ?
+                    <table>
+                        <tbody>
+                            {Object.keys(currentTable).length > 0 ? currentTable.data.map((d) => (
+                                <tr key={nanoid()}>
+                                    <td>{d.key}</td>
+                                    <td>
+                                        <strong>{formatNumber(d.value)}</strong>
+                                    </td>
+                                </tr>
+                            )) : ''}
+                        </tbody>
+                    </table> : null
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Table
